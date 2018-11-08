64776fb37d4d0704979822c122570be6f017ab07
Kasper Andersen <kga@live.dk>
2018-11-08 09:46:00 +0100

    yeahyeah

---
+++ semester4/azure ex/azure ex/src/TaskList.Core/Helpers/APIKeys.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace TaskList.Core
+{
+    public static class APIKeys
+    {
+//#error Enter the connection string you find in the Portal first
+        public static readonly string ConnectionString = "mongodb://mytasks:JVgr6lbnS1hsbqOA1FeJpm6mLzDQ4BXUJfj4GZ448UXGVG7Qe6rGc4tv2TbxVL6DrFEGzfu8QNd1r1SLGyzcRA==@mytasks.documents.azure.com:10255/?ssl=true&replicaSet=globaldb";
+    }
+}
