3a74340005dce45fabb9f8482dd23872c7e3f57d
kasper andersen <kga@live.dk>
2018-11-08 09:53:22 +0100

    yeyayayayay

---
+++ EventTests/QuizManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerTest.Model
+{
+    public class QuizManager
+    {
+
+        //need database util
+        public Quiz _CurrentQuiz { get; set; }
+
+        public void LoadQuiz(int quizID)
+        {
+            _CurrentQuiz = new Quiz();
+            _CurrentQuiz._QuizID = quizID;
+
+                //util.GetQuiz(ref Quiz)
+            
+
+        }
+
+
+
+        //If Necessary Add Load Option and LoadQuestion
+    }
+}
---
+++ TimerTest/Model/Quiz.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerTest.Model
+{
+    public class Quiz
+    {
+        public string  _QuizName { get; set; }
+        public int _QuizID { get; set; }
+        public int _TopicID { get; set; }
+        public double _TotalScore { get; set; }
+        public IList<Question> _QuestionList { get; set; }
+
+        public Quiz()
+        {
+            Question q = new Question();
+            q.S
+        }
+
+        public Quiz(string quizName, int quizID, int topicID)
+        {
+            _QuizName = quizName;
+            _QuizID = quizID;
+            _TopicID = topicID;
+
+            _QuestionList = (IList<Question>)new List<Question>();
+
+        }
+        
+
+
+    }
+}
